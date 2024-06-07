916e238d2f48938ac74bba8a616794008270f9c2
Diego Ugarte <diego.ugarte@outlook.com>
2024-06-07 12:45:17 +0900

    update test for lumi

---
+++ fugaku_droplet_benchmark/crd/tar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+tar -zxvf rCG_Droplets_50_50_50_100_b100_f52_g20_d0_rho0.62.gro.tar.gz
+tar -zxvf rCG_Droplets_50_50_50_100_b100_f52_g20_d0_rho1.00.gro.tar.gz
+tar -zxvf rCG_Droplets_50_50_50_100_b100_f53_g20_d0_rho0.62.gro.tar.gz
+tar -zxvf rCG_Droplets_50_50_50_100_b100_f53_g20_d0_rho1.00.gro.tar.gz
+tar -zxvf rCG_Droplets_50_50_50_100_b100_f76_g20_d0_rho0.62.gro.tar.gz
+tar -zxvf rCG_Droplets_50_50_50_100_b100_f76_g20_d0_rho1.00.gro.tar.gz
+tar -zxvf rCG_Droplets_50_50_50_100_b100_f85_g20_d0_rho0.62.gro.tar.gz
+tar -zxvf rCG_Droplets_50_50_50_100_b100_f85_g20_d0_rho1.00.gro.tar.gz
+tar -zxvf rCG_Droplets_50_50_50_100_b100_f88_g20_d0_rho0.62.gro.tar.gz
+tar -zxvf rCG_Droplets_50_50_50_100_b100_f88_g20_d0_rho1.00.gro.tar.gz
